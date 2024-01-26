10e3b7983ef1ecdc75955a96dc880c19c5040584
Andre Wiggins <andrewiggins@live.com>
2024-01-26 15:24:26 -0800

    Add handling for subpaths in dependency packages

--- cli/src/plugins/dependencyPlugin.js
+++ cli/src/plugins/dependencyPlugin.js
@@ -1,4 +1,7 @@
 import { createRequire } from "node:module";
+import path from "node:path";
+import fs from "node:fs/promises";
+import * as resolve from "resolve.exports";
 import { URLPattern } from "urlpattern-polyfill";
 import { depFilePath, repoRoot, toURLPath } from "../utils.js";
 import { getDepConfig } from "../config.js";
@@ -12,11 +15,52 @@ const require = createRequire(import.meta.url);
 require.extensions[".jsx"] = compileModule;
 require.extensions[".tsx"] = compileModule;
 require.extensions[".ts"] = compileModule;
-const resolve = require.resolve;
 
 const pattern = new URLPattern(
 	"https://localhost:/apps/:appName/:benchmarkName"
 );
+
+const depImportPrefix = `/@dep/`;
+const implImportPrefix = `/@impl`;
+
+/**
+ * Take a bare import specifier and mark it as a dependency our plugin will
+ * handle by prefixing it.
+ * @type {(dep: string) => string}
+ */
+const toDepImportMapId = (dep) => `${depImportPrefix}${dep}`;
+
+/**
+ * For a given dependency and version, generate an import map that maps the
+ * top-level imports into a concrete url path (e.g. `/@dep/preact` with version
+ * `latest` maps to `/dependencies/preact/latest/index.js`). It also adds
+ * mappings for any subpaths defined in the package.json for the dependency.
+ * @type {(dep: string, version: string) => Promise<ImportMap>}
+ */
+async function getImportMapForDep(dep, version) {
+	/** @type {(...args: string[]) => string} */
+	const depDir = (...args) => depFilePath(dep, version, ...args);
+	const pkgPath = depDir("package.json");
+	const pkg = JSON.parse(await fs.readFile(pkgPath, "utf-8"));
+
+	const main = resolve.exports(pkg, ".")?.[0];
+	if (!main) throw new Error(`Could not resolve main for ${dep}@${version}`);
+
+	/** @type {ImportMap} */
+	const importMap = { imports: {} };
+	const importSpecifier = toDepImportMapId(dep);
+	importMap.imports[importSpecifier] = toURLPath(depDir(main));
+
+	for (let subPkg of Object.keys(pkg.exports ?? {})) {
+		const subPkgPath = resolve.exports(pkg, subPkg)?.[0];
+		if (!subPkgPath) continue;
+
+		const importSpecifier = toDepImportMapId(path.posix.join(dep, subPkg));
+		importMap.imports[importSpecifier] = toURLPath(depDir(subPkgPath));
+	}
+
+	return importMap;
+}
 
 /**
  * Plugin to insert an import map into benchmark HTML files to map dependencies
@@ -25,28 +69,24 @@ const pattern = new URLPattern(
  * @type {() => import('vite').Plugin}
  */
 export function dependencyPlugin() {
-	const depImportPrefix = `/@dep/`;
-	const implImportPrefix = `/@impl`;
-
-	/** @type {(dep: string) => string} */
-	const toDepImportMapId = (dep) => `${depImportPrefix}${dep}`;
-
 	return {
 		name: "preact-benchmark:dependency",
 		async resolveId(source, importer) {
 			if (!importer) return;
 			if (!importer.startsWith(repoRoot("apps"))) return;
 
-			// TODO: Maybe precaluclate this when running benchmarks? and do it live
+			// TODO: Maybe precalculate this when running benchmarks? and do it live
 			// in dev mode?
 			const depConfig = await getDepConfig();
+			const dependencies = Object.keys(depConfig);
 
 			if (source === "@impl") {
 				// This is the app import. Map it to the requested implementation
 				return implImportPrefix;
-			} else if (depConfig[source]) {
-				// TODO: `depConfig[source]` won't work for nested imports: `preact/hooks`
-
+			} else if (
+				dependencies.includes(source) ||
+				dependencies.includes(source.split("/")[0])
+			) {
 				// This source url needs to be mapped. Mark for mapping by giving it a
 				// prefix that the importmap will use to map it.
 				return toDepImportMapId(source);
@@ -78,7 +118,7 @@ export function dependencyPlugin() {
 			// Add the app import
 			const impl = params.get("impl") ?? "";
 			importMap.imports[implImportPrefix] = toURLPath(
-				resolve(repoRoot("apps", appName, impl))
+				require.resolve(repoRoot("apps", appName, impl))
 			);
 
 			for (let [key, value] of params) {
@@ -87,15 +127,8 @@ export function dependencyPlugin() {
 				const dep = key.slice(4);
 				const version = value;
 
-				const importMapId = toDepImportMapId(dep);
-				const actualDepUrl = toURLPath(resolve(depFilePath(dep, version)));
-				importMap.imports[importMapId] = actualDepUrl;
-
-				// TODO: What do we need to add here for this? Might need to extend
-				// `getDepConfig` to include subpackage paths
-				//
-				// importMap.imports[importMapId + "/"] =
-				// 	toURLPath(depFilePath(dep, version)) + "/";
+				const depImportMap = await getImportMapForDep(dep, version);
+				importMap.imports = { ...importMap.imports, ...depImportMap.imports };
 			}
 
 			// Serialize import map and fix indentation so it looks nice when debugging
